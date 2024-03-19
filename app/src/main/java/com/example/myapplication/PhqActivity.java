e3cc3dccaf5047c5cfd19a6cb86a34ebf3c46d20
RoxanaGoina <roxana.goina02@e-uvt.ro>
2024-03-19 10:48:36 +0200

    Design bugs error

--- app/src/main/java/com/example/myapplication/PhqActivity.java
+++ app/src/main/java/com/example/myapplication/PhqActivity.java
@@ -3,12 +3,28 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class PhqActivity extends AppCompatActivity {
+    private int score=0;
+    private Button firstQuestionAtAll;
+    private Button firstQuestionSomeDays;
+    private Button firstQuestionHalfDays;
+    private Button firstQuestionAllTheTime;
+
+    private  Button secondQuestionNotAtAll;
+    private  Button secondQuestionSomeDays;
+    private  Button secondQuestionHalfDays;
+    private Button secondQuestAllTheTime;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phq);
+        firstQuestionAtAll=findViewById(R.id.firstQuestionAtAll);
     }
 }
--- app/src/main/java/com/example/myapplication/ResultActivity.java
+++ app/src/main/java/com/example/myapplication/ResultActivity.java
@@ -1,14 +1,58 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class ResultActivity extends AppCompatActivity {
-
+//    private RadioButton radioButtonAnxiety;
+//    private RadioButton radioButtonDepression;
+//    private RadioButton radioButtonDepressionDiagnosis;
+//    private RadioButton radioButtonDepressionTreatment;
+//    private RadioButton radioButtonAnxietyDiagnosis;
+//    private RadioButton radioButtonAnxietyTreatment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+//          radioButtonAnxiety=findViewById(R.id.radioButtonAnxiety);
+//          radioButtonAnxiety.setChecked(false);
+//          radioButtonAnxiety.setTextColor(ContextCompat.getColor(this, R.color.black));
+//          radioButtonAnxiety.setButtonTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.black)));
+//          radioButtonDepression=findViewById(R.id.radioButtonDepression);
+//          radioButtonDepression.setButtonTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.black)));
+//          radioButtonDepressionDiagnosis=findViewById(R.id.radioButtonDepressionDiagnosis);
+//          radioButtonDepressionDiagnosis.setButtonTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.black)));
+//          radioButtonDepressionTreatment=findViewById(R.id.radioButtonDepressionTreatment);
+//          radioButtonDepressionTreatment.setButtonTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.black)));
+//          radioButtonAnxietyDiagnosis=findViewById(R.id.radioButtonAnxietyDiagnosis);
+//          radioButtonAnxietyDiagnosis.setButtonTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.black)));
+//          radioButtonAnxietyTreatment=findViewById(R.id.radioButtonAnxietyTreatment);
+//          radioButtonAnxietyTreatment.setButtonTintList(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.black)));
+//        radioButtonAnxiety.setOnClickListener(new View.OnClickListener() {
+//            boolean isChecked = radioButtonAnxiety.isChecked();
+//
+//            @Override
+//            public void onClick(View v) {
+//                radioButtonAnxiety.setButtonTintList(ColorStateList.valueOf(Color.RED));
+//            }
+//        });
+
+//            radioButtonDepression.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    if(radioButtonDepression.isChecked())
+//                        radioButtonDepression.setChecked(false);
+//                    else
+//                        radio`ButtonDepression.setChecked(true);
+//
+//
+//                }
+//            });
     }
 }
