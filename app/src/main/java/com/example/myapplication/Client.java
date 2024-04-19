d99b8548396c6c1a8c123b7b75fcaa3bda761760
RoxanaGoina <roxana.goina02@e-uvt.ro>
2024-04-19 23:14:01 +0300

    new class for sockets procedure

---
+++ app/src/main/java/com/example/myapplication/Client.java
@@ -0,0 +1,43 @@
+package com.example.myapplication;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+public class Client {
+    public static void main(String[] args) {
+        String host = "127.0.0.1";
+        int port = 12345;
+
+        try (Socket socket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            System.out.println("Connected to server " + host + ":" + port);
+
+
+
+            int[] dataToSend = {2, 24, 1, 1, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+            String jsonData = "[" + arrayToJSONString(dataToSend) + "]";
+
+            out.println(jsonData);
+
+            String receivedData = in.readLine();
+            System.out.println("Received data from server: " + receivedData);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String arrayToJSONString(int[] array) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            sb.append(array[i]);
+            if (i < array.length - 1) {
+                sb.append(",");
+            }
+        }
+        return sb.toString();
+    }
+}
